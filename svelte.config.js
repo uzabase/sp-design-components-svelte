d36cc3f9c8499c8f9ffbfe98758f08b632bf94d6
tsunaki.okubo <tsunaki.okubo@uzabase.com>
2023-03-14 01:39:19 +0900

    css importを使えるようにしたい

--- svelte.config.js
+++ svelte.config.js
@@ -1,7 +1,17 @@
 import preprocess from 'svelte-preprocess';
+import postcss from 'rollup-plugin-postcss'
 
 const config = {
-  preprocess: preprocess({})
+  preprocess: preprocess({
+    build: {
+      rollupOptions: {
+        input: "src/index.ts",
+      },
+      plugins :[
+        postcss({})
+      ]
+    },
+  })
 }
 
 export default config;
