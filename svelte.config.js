f8bd9766b0e7192b941afcd20fc0646818f96335
tsunaki.okubo <tsunaki.okubo@uzabase.com>
2023-03-14 01:26:24 +0900

    インポートの位置変更のテスト

--- svelte.config.js
+++ svelte.config.js
@@ -1,13 +1,7 @@
 import preprocess from 'svelte-preprocess';
 
 const config = {
-  preprocess: preprocess({
-    build: {
-      rollupOptions: {
-        input: 'src/index.ts',
-      },
-    },
-  })
+  preprocess: preprocess({})
 }
 
 export default config;
