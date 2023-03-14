be8cd2a9e963f6bcdade3b403cc87aac28e1f89e
tsunaki.okubo <tsunaki.okubo@uzabase.com>
2023-03-14 14:46:02 +0900

    resetの削除

--- src/index.ts
+++ src/index.ts
@@ -1,2 +1,3 @@
+import "@sp-design/token/styles/speeda-tokens.css";
 export { default as Button } from "./components/Button/Button.svelte";
 export { default as Checkbox } from "./components/Checkbox/Checkbox.svelte";
