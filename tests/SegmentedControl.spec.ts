39c842229a58b18e12f6240cc8efada29af54bc3
Hazuki Okuda <hazuki.okuda@uzabase.com>
2023-05-29 15:21:16 +0900

    buttonのpadding調整他

--- tests/SegmentedControl.spec.ts
+++ tests/SegmentedControl.spec.ts
@@ -2,6 +2,32 @@ import "@testing-library/jest-dom";
 import { render } from "@testing-library/svelte";
 import SegmentedControl from "../src/components/SegmentedControl/SegmentedControl.svelte";
 
+//type=radioである
+test("inputTypeがradioである", () => {
+  const { getAllByTestId } = render(SegmentedControl, {
+    items: [{ label: "<ANY>", value: "<ANY>" }],
+  });
+  expect(
+    (
+      getAllByTestId("segmentedControlRadio")[0] as HTMLInputElement
+    ).getAttribute("type")
+  ).toBe("radio");
+});
+
+test("各inputが同じグループである", () => {
+  const { getAllByTestId } = render(SegmentedControl, {
+    items: [
+      { label: "<ANY>", value: "<ANY>" },
+      { label: "<ANY>", value: "<ANY>" },
+    ],
+  });
+  const element1 = getAllByTestId("segmentedControlRadio")[0];
+  const element2 = getAllByTestId("segmentedControlRadio")[1];
+  expect((element1 as HTMLInputElement).getAttribute("name")).toBe(
+    (element2 as HTMLInputElement).getAttribute("name")
+  );
+});
+
 //SegmentedControlのボタンにlabelが表示されている
 test("SegmentedControlのボタンにlabelが表示されている", () => {
   const { getAllByTestId } = render(SegmentedControl, {
@@ -21,7 +47,7 @@ test("SegmentedControlのボタンにvalueが入っている", () => {
 });
 
 //itemsの数と表示されているボタンの数が一緒である
-test("hoghogehoge", () => {
+test("itemsの数と表示されているボタンの数が一緒である", () => {
   const { getAllByTestId } = render(SegmentedControl, {
     items: [
       { label: "<ANY>", value: "<ANY>" },
