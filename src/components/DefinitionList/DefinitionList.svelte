ceb71fee64461c56eff8974918640d94cd4a9e8e
tsunaki.okubo <tsunaki.okubo@uzabase.com>
2023-05-31 11:25:36 +0900

    DefinitionListにclass追加

--- src/components/DefinitionList/DefinitionList.svelte
+++ src/components/DefinitionList/DefinitionList.svelte
@@ -1,8 +1,10 @@
 <script lang="ts">
   export let size: "width80" | "width160" | "width240" = "width160";
+  let clazz: string | undefined = undefined;
+  export { clazz as class };
 </script>
 
-<dl class="base {size}">
+<dl class="base {size} {clazz ? clazz : ''}">
   <slot />
 </dl>
 
